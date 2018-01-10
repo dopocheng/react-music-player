a45bd5c69b15082ff10f4b629990cca7c043b519
dopocheng <747015697@qq.com>
2018-01-10 21:24:51 +0800

    修改文件
    root修改测试列表页面
    package加个依赖

--- app/root.js
+++ app/root.js
@@ -2,16 +2,19 @@ import React from 'react';
 import Header from './components/header';
 import Progress from './components/Progress';
 import 'jplayer';
-import Player from '../pages/Player';
+import Player from './pages/Player';
+import Musiclist from './pages/musiclist';
+import { MUSIC_LIST } from './config/musiclist';
 
 let duration = null;
 class Root extends React.Component{
-	// constructor() {
-	//     super();
-	//     this.state = {
-	//       progress: '-'
-	//     };
- //    }
+	constructor() {
+	    super();
+	    this.state = {
+	    	musicList: MUSIC_LIST,
+	      currentMusicItem: MUSIC_LIST[3]
+	    };
+    }
 	// getInitialState() {
 	// 	return {
 	// 		progress: '-'
@@ -25,6 +28,7 @@ class Root extends React.Component{
 				$('#player').jPlayer('setMedia', {
 					mp3: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
 				}).jPlayer('play');
+				}).jPlayer('pause');
 			},
 			supplied: 'mp3',
 			wmode: 'window'
@@ -50,7 +54,10 @@ class Root extends React.Component{
 		return (
 			<div>
 				<Header />
-				<Player />
+				<Musiclist 
+					currentMusicItem={this.state.currentMusicItem}
+					musicList={this.state.musicList}
+				 />
 			</div>	
 		);
 	}
