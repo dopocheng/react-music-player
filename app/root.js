a280afc40c34f19764654d4067374e4bb0cf1cb0
dopocheng <747015697@qq.com>
2018-01-10 21:27:30 +0800

    修改
    修改

--- app/root.js
+++ app/root.js
@@ -27,7 +27,6 @@ class Root extends React.Component{
 			ready: function () {
 				$('#player').jPlayer('setMedia', {
 					mp3: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
-				}).jPlayer('play');
 				}).jPlayer('pause');
 			},
 			supplied: 'mp3',
