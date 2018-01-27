453faf0ca6d5bfe87b5ca05d9c59c0a2f7416777
dopocheng <747015697@qq.com>
2018-01-27 16:30:11 +0800

    list列表
    列表的点击事件

--- app/components/musiclistitem.js
+++ app/components/musiclistitem.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './musiclistitem.less';
+import Pubsub from 'Pubsub-js';
 
 class MusicListItem extends React.Component{
+	playerMusic(musicItem) {
+		Pubsub.publish('PLAYER_MUSIC', musicItem);
+	};
+
+	deleteMusic(musicItem, e) {
+		//阻止事件冒泡
+		e.stopPropagation();
+		Pubsub.publish('DELETE_MUSIC', musicItem)
+	};
+
 	render() {
 		let musicItem = this.props.musicItem;
 		return (
 			<li className={`components-listitem row${this.props.focus ? ' focus' : ''}`}>
+			<li onClick={this.playerMusic.bind(this, musicItem)} className={`components-listitem row${this.props.focus ? ' focus' : ''}`}>
 				<p><strong>{musicItem.title} </strong> - {musicItem.artist} </p>
-				<p className="-col-auto delete"></p>
+				<p onClick={this.deleteMusic.bind(this, musicItem)} className="-col-auto delete"></p>
 			</li>
 			)
 	}
